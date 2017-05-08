952430adeb88283588e07efc8079318bfb586623
Feng Zhou <zhoufengfox@gmail.com>
2017-05-08 23:31:00 +0800

    import material ui

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          周峰的博客 <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
